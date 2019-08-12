83fdc6c3034bcf3b1d81c9cf631320d2f8168594
nvthongswansea <nvthongswansea@gmail.com>
2019-08-12 17:34:36 +0200

    Create serverisoimage_test.go

---
+++ serverisoimage_test.go
@@ -0,0 +1,139 @@
+package gsclient
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"path"
+	"testing"
+)
+
+func TestClient_GetServerIsoImageList(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages")
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodGet, request.Method)
+		fmt.Fprintf(writer, prepareServerIsoImageListHTTPGet())
+	})
+	res, err := client.GetServerIsoImageList(dummyUuid)
+	if err != nil {
+		t.Errorf("GetServerIsoImageList returned an error %v", err)
+	}
+	assert.Equal(t, 1, len(res))
+	assert.Equal(t, fmt.Sprintf("[%v]", getMockServerIsoImage()), fmt.Sprintf("%v", res))
+}
+
+func TestClient_GetServerIsoImage(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages", dummyUuid)
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodGet, request.Method)
+		fmt.Fprintf(writer, prepareServerIsoImageHTTPget())
+	})
+	res, err := client.GetServerIsoImage(dummyUuid, dummyUuid)
+	if err != nil {
+		t.Errorf("GetServerIsoImage returned an error %v", err)
+	}
+	assert.Equal(t, fmt.Sprintf("%v", getMockServerIsoImage()), fmt.Sprintf("%v", res))
+}
+
+func TestClient_CreateServerIsoImage(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages")
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodPost, request.Method)
+		fmt.Fprint(writer, "")
+	})
+	err := client.CreateServerIsoImage(dummyUuid, ServerIsoImageCreateRequest{
+		ObjectUuid:dummyUuid,
+	})
+	if err != nil {
+		t.Errorf("CreateServerIsoImage returned an error %v", err)
+	}
+}
+
+func TestClient_UpdateServerIsoImage(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages", dummyUuid)
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodPatch, request.Method)
+		fmt.Fprint(writer, "")
+	})
+	err := client.UpdateServerIsoImage(dummyUuid, dummyUuid, ServerIsoImageUpdateRequest{
+		BootDevice: true,
+		Name:       "test",
+	})
+	if err != nil {
+		t.Errorf("UpdateServerIsoImage returned an error %v", err)
+	}
+}
+
+func TestClient_DeleteServerIsoImage(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages", dummyUuid)
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodDelete, request.Method)
+		fmt.Fprint(writer, "")
+	})
+	err := client.DeleteServerIsoImage(dummyUuid, dummyUuid)
+	if err != nil {
+		t.Errorf("DeleteServerIsoImage returned an error %v", err)
+	}
+}
+
+func TestClient_LinkIsoimage(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages")
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodPost, request.Method)
+		fmt.Fprint(writer, "")
+	})
+	err := client.LinkIsoimage(dummyUuid, dummyUuid)
+	if err != nil {
+		t.Errorf("LinkIsoimage returned an error %v", err)
+	}
+}
+
+func TestClient_UnlinkIsoimage(t *testing.T) {
+	server, client, mux := setupTestClient()
+	defer server.Close()
+	uri := path.Join(apiServerBase, dummyUuid, "isoimages", dummyUuid)
+	mux.HandleFunc(uri, func(writer http.ResponseWriter, request *http.Request) {
+		assert.Equal(t, http.MethodDelete, request.Method)
+		fmt.Fprint(writer, "")
+	})
+	err := client.UnlinkIsoimage(dummyUuid, dummyUuid)
+	if err != nil {
+		t.Errorf("UnlinkIsoimage returned an error %v", err)
+	}
+}
+
+func getMockServerIsoImage() ServerIsoImage {
+	mock := ServerIsoImage{
+		ObjectUuid: dummyUuid,
+		ObjectName: "test",
+		Private:    false,
+		CreateTime: dummyTime,
+		Bootdevice: true,
+	}
+	return mock
+}
+
+func prepareServerIsoImageListHTTPGet() string {
+	iso := getMockServerIsoImage()
+	res, _ := json.Marshal(iso)
+	return fmt.Sprintf(`{"isoimage_relations": [%s]}`, string(res))
+}
+
+func prepareServerIsoImageHTTPget() string {
+	iso := getMockServerIsoImage()
+	res, _ := json.Marshal(iso)
+	return fmt.Sprintf(`{"isoimage_relation": %s}`, string(res))
+}
