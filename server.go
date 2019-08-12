cee520eb835f62805695dc4b1230d4c5aaec17d4
nvthongswansea <nvthongswansea@gmail.com>
2019-08-12 15:54:10 +0200

    Update coding convention

--- server.go
+++ server.go
@@ -1,5 +1,10 @@
 package gsclient
 
+import (
+	"net/http"
+	"path"
+)
+
 type Servers struct {
 	List map[string]ServerProperties `json:"servers"`
 }
@@ -95,8 +100,21 @@ type ServerCreateRequest struct {
 	LocationUuid    string                       `json:"location_uuid"`
 	HardwareProfile string                       `json:"hardware_profile,omitempty"`
 	AvailablityZone string                       `json:"availability_zone,omitempty"`
-	Labels          []interface{}                `json:"labels,omitempty"`
+	Labels          []string                     `json:"labels,omitempty"`
 	Relations       ServerCreateRequestRelations `json:"relations,omitempty"`
+}
+
+type ServerCreateResponse struct {
+	ObjectUuid   string   `json:"object_uuid"`
+	RequestUuid  string   `json:"request_uuid"`
+	SeverUuid    string   `json:"sever_uuid"`
+	NetworkUuids []string `json:"network_uuids"`
+	StorageUuids []string `json:"storage_uuids"`
+	IpaddrUuids  []string `json:"ipaddr_uuids"`
+}
+
+type ServerPowerUpdateRequest struct {
+	Power bool `json:"power"`
 }
 
 type ServerCreateRequestRelations struct {
@@ -125,56 +143,100 @@ type ServerCreateRequestIsoimage struct {
 }
 
 type ServerUpdateRequest struct {
-	Name            string        `json:"name,omitempty"`
-	AvailablityZone string        `json:"availability_zone,omitempty"`
-	Memory          int           `json:"memory,omitempty"`
-	Cores           int           `json:"cores,omitempty"`
-	Labels          []interface{} `json:"labels"`
+	Name            string   `json:"name,omitempty"`
+	AvailablityZone string   `json:"availability_zone,omitempty"`
+	Memory          int      `json:"memory,omitempty"`
+	Cores           int      `json:"cores,omitempty"`
+	Labels          []string `json:"labels"`
 }
 
-func (c *Client) GetServer(id string) (*Server, error) {
+type ServerEventList struct {
+	List []ServerEventProperties `json:"events"`
+}
+
+type ServerEvent struct {
+	Properties ServerEventProperties `json:"event"`
+}
+
+type ServerEventProperties struct {
+	ObjectType    string `json:"object_type"`
+	RequestUuid   string `json:"request_uuid"`
+	ObjectUuid    string `json:"object_uuid"`
+	Activity      string `json:"activity"`
+	RequestType   string `json:"request_type"`
+	RequestStatus string `json:"request_status"`
+	Change        string `json:"change"`
+	Timestamp     string `json:"timestamp"`
+	UserUuid      string `json:"user_uuid"`
+}
+
+type ServerMetricList struct {
+	List []ServerMetricProperties `json:"server_metrics"`
+}
+
+type ServerMetric struct {
+	Properties ServerMetricProperties `json:"server_metric"`
+}
+
+type ServerMetricProperties struct {
+	BeginTime       string `json:"begin_time"`
+	EndTime         string `json:"end_time"`
+	PaaSServiceUuid string `json:"paas_service_uuid"`
+	CoreUsage       struct {
+		Value float64 `json:"value"`
+		Unit  string  `json:"unit"`
+	} `json:"core_usage"`
+	StorageSize struct {
+		Value float64 `json:"value"`
+		Unit  string  `json:"unit"`
+	} `json:"storage_size"`
+}
+
+//GetServer gets a specific server based on given list
+func (c *Client) GetServer(id string) (Server, error) {
 	r := Request{
-		uri:    apiServerBase + "/" + id,
-		method: "GET",
+		uri:    path.Join(apiServerBase, id),
+		method: http.MethodGet,
 	}
 
-	response := new(Server)
+	var response Server
 	err := r.execute(*c, &response)
 
 	return response, err
 }
 
+//GetServerList gets a list of available servers
 func (c *Client) GetServerList() ([]Server, error) {
 	r := Request{
 		uri:    apiServerBase,
-		method: "GET",
+		method: http.MethodGet,
 	}
 
-	response := new(Servers)
+	var response Servers
 	err := r.execute(*c, &response)
 
 	list := []Server{}
 	for _, properties := range response.List {
-		server := Server{
+		list = append(list, Server{
 			Properties: properties,
-		}
-		list = append(list, server)
+		})
 	}
 
 	return list, err
 }
 
-func (c *Client) CreateServer(body ServerCreateRequest) (*CreateResponse, error) {
+//CreateServer create a server
+func (c *Client) CreateServer(body ServerCreateRequest) (ServerCreateResponse, error) {
 	r := Request{
 		uri:    apiServerBase,
-		method: "POST",
+		method: http.MethodPost,
 		body:   body,
 	}
 
-	response := new(CreateResponse)
+	var response ServerCreateResponse
 	err := r.execute(*c, &response)
 	if err != nil {
-		return nil, err
+		return ServerCreateResponse{}, err
 	}
 
 	err = c.WaitForRequestCompletion(response.RequestUuid)
@@ -182,41 +244,83 @@ func (c *Client) CreateServer(body ServerCreateRequest) (*CreateResponse, error)
 	return response, err
 }
 
+//DeleteServer deletes a specific server
 func (c *Client) DeleteServer(id string) error {
 	r := Request{
-		uri:    apiServerBase + "/" + id,
-		method: "DELETE",
+		uri:    path.Join(apiServerBase, id),
+		method: http.MethodDelete,
 	}
 
 	return r.execute(*c, nil)
 }
 
+//UpdateServer updates a specific server
 func (c *Client) UpdateServer(id string, body ServerUpdateRequest) error {
 	r := Request{
-		uri:    apiServerBase + "/" + id,
-		method: "PATCH",
+		uri:    path.Join(apiServerBase, id),
+		method: http.MethodPatch,
 		body:   body,
 	}
 
 	return r.execute(*c, nil)
 }
 
-func (c *Client) StopServer(id string) error {
+//GetServerEventList gets a list of a specific server's events
+func (c *Client) GetServerEventList(id string) ([]ServerEvent, error) {
+	r := Request{
+		uri:    path.Join(apiServerBase, id, "events"),
+		method: http.MethodGet,
+	}
+	var response ServerEventList
+	var list []ServerEvent
+	err := r.execute(*c, &response)
+	for _, properties := range response.List {
+		list = append(list, ServerEvent{Properties: properties})
+	}
+	return list, err
+}
+
+//GetServerMetricList gets a list of a specific server's metrics
+func (c *Client) GetServerMetricList(id string) ([]ServerMetric, error) {
+	r := Request{
+		uri:    path.Join(apiServerBase, id, "metrics"),
+		method: http.MethodGet,
+	}
+	var response ServerMetricList
+	var list []ServerMetric
+	err := r.execute(*c, &response)
+	for _, properties := range response.List {
+		list = append(list, ServerMetric{Properties: properties})
+	}
+	return list, err
+}
+
+//IsServerOn returns true if the server's power is on, otherwise returns false
+func (c *Client) IsServerOn(id string) (bool, error) {
 	server, err := c.GetServer(id)
+	if err != nil {
+		return false, err
+	}
+	return server.Properties.Power, nil
+}
+
+//TurnOnOffServer turn on/off a specific server.
+//turnOn=true to turn on, turnOn=false to turn off
+func (c *Client) TurnOnOffServer(id string, turnOn bool) error {
+	isOn, err := c.IsServerOn(id)
 	if err != nil {
 		return err
 	}
-	if !server.Properties.Power {
+	if isOn == turnOn {
 		return nil
 	}
 
-	body := map[string]interface{}{
-		"power": false,
-	}
 	r := Request{
-		uri:    apiServerBase + "/" + id + "/power",
-		method: "PATCH",
-		body:   body,
+		uri:    path.Join(apiServerBase, id, "power"),
+		method: http.MethodPatch,
+		body: ServerPowerUpdateRequest{
+			Power: turnOn,
+		},
 	}
 
 	err = r.execute(*c, nil)
@@ -224,9 +328,10 @@ func (c *Client) StopServer(id string) error {
 		return err
 	}
 
-	return c.WaitForServerPowerStatus(id, false)
+	return c.WaitForServerPowerStatus(id, turnOn)
 }
 
+//ShutdownServer shutdowns a specific server
 func (c *Client) ShutdownServer(id string) error {
 	//Make sure the server exists and that it isn't already in the state we need it to be
 	server, err := c.GetServer(id)
@@ -238,8 +343,8 @@ func (c *Client) ShutdownServer(id string) error {
 	}
 
 	r := Request{
-		uri:    apiServerBase + "/" + id + "/shutdown",
-		method: "PATCH",
+		uri:    path.Join(apiServerBase, id, "shutdown"),
+		method: http.MethodPatch,
 		body:   new(map[string]string),
 	}
 
@@ -251,124 +356,8 @@ func (c *Client) ShutdownServer(id string) error {
 	//If we get an error, which includes a timeout, power off the server instead
 	err = c.WaitForServerPowerStatus(id, false)
 	if err != nil {
-		return c.StopServer(id)
+		return c.TurnOnOffServer(id, false)
 	}
 
 	return nil
-}
-
-func (c *Client) StartServer(id string) error {
-	server, err := c.GetServer(id)
-	if err != nil {
-		return err
-	}
-	if server.Properties.Power {
-		return nil
-	}
-
-	body := map[string]interface{}{
-		"power": true,
-	}
-	r := Request{
-		uri:    apiServerBase + "/" + id + "/power",
-		method: "PATCH",
-		body:   body,
-	}
-
-	err = r.execute(*c, nil)
-	if err != nil {
-		return err
-	}
-
-	return c.WaitForServerPowerStatus(id, true)
-}
-
-func (c *Client) LinkStorage(serverid string, storageid string, bootdevice bool) error {
-	body := map[string]interface{}{
-		"object_uuid": storageid,
-		"bootdevice":  bootdevice,
-	}
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/storages",
-		method: "POST",
-		body:   body,
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) UnlinkStorage(serverid string, storageid string) error {
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/storages/" + storageid,
-		method: "DELETE",
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) LinkNetwork(serverid string, networkid string, bootdevice bool) error {
-	body := map[string]interface{}{
-		"object_uuid": networkid,
-		"bootdevice":  bootdevice,
-	}
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/networks",
-		method: "POST",
-		body:   body,
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) UnlinkNetwork(serverid string, networkid string) error {
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/networks/" + networkid,
-		method: "DELETE",
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) LinkIsoimage(serverid string, isoimageid string) error {
-	body := map[string]interface{}{
-		"object_uuid": isoimageid,
-	}
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/isoimages",
-		method: "POST",
-		body:   body,
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) UnlinkIsoimage(serverid string, isoimageid string) error {
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/isoimages/" + isoimageid,
-		method: "DELETE",
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) LinkIp(serverid string, ipid string) error {
-	body := map[string]interface{}{
-		"object_uuid": ipid,
-	}
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/ips",
-		method: "POST",
-		body:   body,
-	}
-
-	return r.execute(*c, nil)
-}
-
-func (c *Client) UnlinkIp(serverid string, ipid string) error {
-	r := Request{
-		uri:    apiServerBase + "/" + serverid + "/ips/" + ipid,
-		method: "DELETE",
-	}
-
-	return r.execute(*c, nil)
 }
