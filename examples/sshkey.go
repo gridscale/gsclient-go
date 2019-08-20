bd4d4ba3f595c2f3b8ecadefb039b3b1b0114ea1
nvthongswansea <nvthongswansea@gmail.com>
2019-08-20 09:57:09 +0200

    update coding convention

--- examples/sshkey.go
+++ examples/sshkey.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"bufio"
-	"os"
-	log "github.com/sirupsen/logrus"
 	"github.com/gridscale/gsclient-go"
+	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 const LocationUuid = "45ed677b-3702-4b36-be2a-a2eab9827950"
+
 //exampleSSHkey is an example of SSH-key, don't use it in production
 const exampleSSHkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9BlRsUvqRNKi59UkQmmztP5g+1jX5Ettr9C0+udwu9ATOukoM3rr0dXGVEVOJKQO1QCoEvMxn5HhZO2+klTVC1inapOrFrlUveqhcXvx6Fr1l3AmBsgY7loa5ELgi0qcKNcM/c9J7gB3EadKei/kfo5EXLDchn8SGHEq9Rhi8n8RcpGCEFnuvbao7uRsSj1QxTBaZgl5FL+W7wq2/dtwNhUk/KVA+ZKkMd4EnVlkF2ngQ02WQsu+0TN1gusMhBfph5sqtFT0twoOvYE3ejVaCc5LwT+5oxZulQ4TvggbJjzGD618q0QFkJ0CUtuh2s0otJkx1RqABX3TjfgmDjA8L example@gridscales.local"
 
@@ -21,7 +22,7 @@ func main() {
 	log.Info("Create SSH-key: Press 'Enter' to continue...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 	cSSHkey, err := client.CreateSshkey(gsclient.SshkeyCreateRequest{
-		Name: "go-client-ssh-key",
+		Name:   "go-client-ssh-key",
 		Sshkey: exampleSSHkey,
 	})
 	if err != nil {
