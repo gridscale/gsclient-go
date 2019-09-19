da8540731803745a2db7a40382ff7d761dc80f64
Tom <nvthongswansea@gmail.com>
2019-09-19 16:19:56 +0200

    More detailed comments for `label`

--- label.go
+++ label.go
@@ -8,29 +8,43 @@ import (
 
 //LabelList JSON struct of a list of labels
 type LabelList struct {
+	//List of labels
 	List map[string]LabelProperties `json:"labels"`
 }
 
 //Label JSON struct of a single label
 type Label struct {
+	//Properties of a label
 	Properties LabelProperties `json:"label"`
 }
 
 //LabelProperties JSON struct of properties of a label
 type LabelProperties struct {
-	Label      string        `json:"label"`
-	CreateTime GSTime        `json:"create_time"`
-	ChangeTime GSTime        `json:"change_time"`
-	Relations  []interface{} `json:"relations"`
-	Status     string        `json:"status"`
+	//Label's name
+	Label string `json:"label"`
+
+	//Create time of a label
+	CreateTime GSTime `json:"create_time"`
+
+	//Time of the last change of a label
+	ChangeTime GSTime `json:"change_time"`
+
+	//Relations of a label
+	Relations []interface{} `json:"relations"`
+
+	//Status indicates the status of a label.
+	Status string `json:"status"`
 }
 
 //LabelCreateRequest JSON struct of a request for creating a label
 type LabelCreateRequest struct {
+	//Name of the new label
 	Label string `json:"label"`
 }
 
 //GetLabelList gets a list of available labels
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/GetLabels
 func (c *Client) GetLabelList() ([]Label, error) {
 	r := Request{
 		uri:    apiLabelBase,
@@ -46,6 +60,8 @@ func (c *Client) GetLabelList() ([]Label, error) {
 }
 
 //CreateLabel creates a new label
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/CreateLabel
 func (c *Client) CreateLabel(body LabelCreateRequest) (CreateResponse, error) {
 	r := Request{
 		uri:    apiLabelBase,
@@ -62,6 +78,8 @@ func (c *Client) CreateLabel(body LabelCreateRequest) (CreateResponse, error) {
 }
 
 //DeleteLabel deletes a label
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/DeleteLabel
 func (c *Client) DeleteLabel(label string) error {
 	if label == "" {
 		return errors.New("'label' is required")
