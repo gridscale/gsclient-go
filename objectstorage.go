3038e80ce6afe77fd06981c24ac4643dc7dc9f79
nvthongswansea <nvthongswansea@gmail.com>
2019-09-20 10:11:37 +0200

    add more detailed comments for `objectstorage`

--- objectstorage.go
+++ objectstorage.go
@@ -9,50 +9,72 @@ import (
 
 //ObjectStorageAccessKeyList is JSON structure of a list of Object Storage Access Keys
 type ObjectStorageAccessKeyList struct {
+	//Array of Object Storages' access keys
 	List []ObjectStorageAccessKeyProperties `json:"access_keys"`
 }
 
 //ObjectStorageAccessKey is JSON structure of a single Object Storage Access Key
 type ObjectStorageAccessKey struct {
+	//Properties of an object storage access key
 	Properties ObjectStorageAccessKeyProperties `json:"access_key"`
 }
 
 //ObjectStorageAccessKeyProperties is JSON struct of properties of an object storage access key
 type ObjectStorageAccessKeyProperties struct {
+	//The object storage secret_key.
 	SecretKey string `json:"secret_key"`
+
+	//The object storage access_key.
 	AccessKey string `json:"access_key"`
-	User      string `json:"user"`
+
+	//Account this credentials belong to.
+	User string `json:"user"`
 }
 
 //ObjectStorageAccessKeyCreateResponse is JSON struct of a response for creating an object storage access key
 type ObjectStorageAccessKeyCreateResponse struct {
 	AccessKey struct {
+		////The object storage secret_key.
 		SecretKey string `json:"secret_key"`
+
+		//The object storage secret_key.
 		AccessKey string `json:"access_key"`
 	} `json:"access_key"`
+
+	//UUID of the request
 	RequestUUID string `json:"request_uuid"`
 }
 
 //ObjectStorageBucketList is JSON struct of a list of buckets
 type ObjectStorageBucketList struct {
+	//Array of Buckets
 	List []ObjectStorageBucketProperties `json:"buckets"`
 }
 
 //ObjectStorageBucket is JSON struct of a single bucket
 type ObjectStorageBucket struct {
+	//Properties of a bucket
 	Properties ObjectStorageBucketProperties `json:"bucket"`
 }
 
 //ObjectStorageBucketProperties is JSON struct of properties of a bucket
 type ObjectStorageBucketProperties struct {
+	//The human-readable name of the object. It supports the full UTF-8 charset, with a maximum of 64 characters.
 	Name  string `json:"name"`
+
+	//The current usage of the bucket.
 	Usage struct {
+		//The size of the the bucket (in kb).
 		SizeKb     int `json:"size_kb"`
+
+		//The number of files in the bucket.
 		NumObjects int `json:"num_objects"`
 	} `json:"usage"`
 }
 
 //GetObjectStorageAccessKeyList gets a list of available object storage access keys
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/getAccessKeys
 func (c *Client) GetObjectStorageAccessKeyList() ([]ObjectStorageAccessKey, error) {
 	r := Request{
 		uri:    path.Join(apiObjectStorageBase, "access_keys"),
@@ -68,6 +90,8 @@ func (c *Client) GetObjectStorageAccessKeyList() ([]ObjectStorageAccessKey, erro
 }
 
 //GetObjectStorageAccessKey gets a specific object storage access key based on given id
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/getAccessKey
 func (c *Client) GetObjectStorageAccessKey(id string) (ObjectStorageAccessKey, error) {
 	if strings.TrimSpace(id) == "" {
 		return ObjectStorageAccessKey{}, errors.New("'id' is required")
@@ -82,6 +106,8 @@ func (c *Client) GetObjectStorageAccessKey(id string) (ObjectStorageAccessKey, e
 }
 
 //CreateObjectStorageAccessKey creates an object storage access key
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/createAccessKey
 func (c *Client) CreateObjectStorageAccessKey() (ObjectStorageAccessKeyCreateResponse, error) {
 	r := Request{
 		uri:    path.Join(apiObjectStorageBase, "access_keys"),
@@ -97,6 +123,8 @@ func (c *Client) CreateObjectStorageAccessKey() (ObjectStorageAccessKeyCreateRes
 }
 
 //DeleteObjectStorageAccessKey deletes a specific object storage access key based on given id
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/deleteAccessKey
 func (c *Client) DeleteObjectStorageAccessKey(id string) error {
 	if strings.TrimSpace(id) == "" {
 		return errors.New("'id' is required")
@@ -109,6 +137,8 @@ func (c *Client) DeleteObjectStorageAccessKey(id string) error {
 }
 
 //GetObjectStorageBucketList gets a list of object storage buckets
+//
+//See: https://gridscale.io/en//api-documentation/index.html#operation/getBuckets
 func (c *Client) GetObjectStorageBucketList() ([]ObjectStorageBucket, error) {
 	r := Request{
 		uri:    path.Join(apiObjectStorageBase, "buckets"),
