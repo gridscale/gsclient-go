aca3beea9742d1625e2449e2160cbebfaf071bdc
nvthongswansea <nvthongswansea@gmail.com>
2019-09-27 16:47:59 +0200

    add `retryWithTimeout` and `retryWithLimitedNumOfRetries` functions

--- common.go
+++ common.go
@@ -1,9 +1,52 @@
 package gsclient
 
-import "github.com/google/uuid"
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+type isContinue func() (bool, error)
 
 //isValidUUID validates the uuid
 func isValidUUID(u string) bool {
 	_, err := uuid.Parse(u)
 	return err == nil
+}
+
+//retryWithTimeout reruns a function within a period of time
+func retryWithTimeout(targetFunc isContinue, timeout, delay time.Duration) error {
+	timer := time.After(timeout)
+	for {
+		select {
+		case <-timer:
+			return errors.New("timeout reached when retrying")
+		default:
+			time.Sleep(delay) //delay between retries
+			continueRetrying, err := targetFunc()
+			if err != nil {
+				return err
+			}
+			if !continueRetrying {
+				return nil
+			}
+		}
+	}
+}
+
+//retryWithLimitedNumOfRetries reruns a function within a number of retries
+func retryWithLimitedNumOfRetries(targetFunc isContinue, numOfRetries int, delay time.Duration) error {
+	retryNo := 0
+	for retryNo <= numOfRetries {
+		time.Sleep(delay) //delay between retries
+		continueRetrying, err := targetFunc()
+		if err != nil {
+			return err
+		}
+		if !continueRetrying {
+			return nil
+		}
+		retryNo++
+	}
+	return errors.New("maximum number of retries reached when retrying")
 }
