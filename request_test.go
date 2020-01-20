0c53c3c57bb81aedc3364002073cd57b2114435e
Ibrahim Takouna <ibrahim@gridscale.io>
2020-01-20 12:13:09 +0100

    Add network test case for POST requests

--- request_test.go
+++ request_test.go
@@ -18,39 +18,83 @@ type networkTestCase struct {
 	expectedError string
 }
 
-var networkErrorTests []networkTestCase = []networkTestCase{
+var getNetworkErrorTests []networkTestCase = []networkTestCase{
 	{
-		name: "Connection timeout",
-		apiURL: defaultAPIURL,
+		name: "rety the GET requet in case of connection timeout",
+		apiURL: "http://127.0.0.1",
 		httpClient: &http.Client{Timeout: 1 * time.Nanosecond},
-		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: net/http: request canceled (Client.Timeout exceeded while awaiting headers)",
+		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: net/http: request canceled while waiting for connection (Client.Timeout exceeded while awaiting headers)",
 	},
 	{
-		name: "Connection refused",
+		name: "rety the GET requet in case of connection refused",
 		apiURL: "http://127.0.0.1",
 		httpClient: http.DefaultClient,
 		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: dial tcp 127.0.0.1:80: connect: connection refused",
 	},
 	{
-		name: "DNS lookup error",
+		name: "rety the GET requet in case of DNS lookup error",
 		apiURL: "http://api.unkown.domain",
 		httpClient: http.DefaultClient,
 		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: dial tcp: lookup api.unkown.domain: no such host",
 	},
 }
 
-func TestRequest_RetryNetworkErrors(t *testing.T) {
+var postNetworkErrorTests []networkTestCase = []networkTestCase{
+	{
+		name: "do not rety the POST requet in case of connection timeout",
+		apiURL: "http://127.0.0.1",
+		httpClient: &http.Client{Timeout: 1 * time.Nanosecond},
+		expectedError: "Post %s%s: net/http: request canceled while waiting for connection (Client.Timeout exceeded while awaiting headers)",
+	},
+	{
+		name: "rety the POST requet in case of connection refused",
+		apiURL: "http://127.0.0.1",
+		httpClient: http.DefaultClient,
+		expectedError: "Maximum number of trials has been exhausted with error: Post %s%s: dial tcp 127.0.0.1:80: connect: connection refused",
+	},
+	{
+		name: "rety the POST requet in case of DNS lookup error",
+		apiURL: "http://api.unkown.domain",
+		httpClient: http.DefaultClient,
+		expectedError: "Maximum number of trials has been exhausted with error: Post %s%s: dial tcp: lookup api.unkown.domain: no such host",
+	},
+}
+
+func TestGetRequest_NetworkErrors(t *testing.T) {
 	mux := http.NewServeMux()
 	server := httptest.NewServer(mux)
 	uri := path.Join(apiServerBase, dummyUUID)
 	mux.HandleFunc(uri, func(w http.ResponseWriter, r *http.Request) {})
 	defer server.Close()
 	
-	for _, test := range networkErrorTests {
+	for _, test := range getNetworkErrorTests {
 		config := NewConfiguration(test.apiURL, "uuid", "token", true, true, 1, 100, 5)
 		config.httpClient = test.httpClient
 		client := NewClient(config)
 		_, err := client.GetServer(emptyCtx, dummyUUID)
+		assert.EqualError(t, err, fmt.Sprintf(test.expectedError, config.apiURL, uri), test.name)
+	}
+}
+
+func TestPostRequest_NetworkErrors(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	uri := apiServerBase
+	mux.HandleFunc(uri, func(w http.ResponseWriter, r *http.Request) {})
+	defer server.Close()
+	
+	for _, test := range postNetworkErrorTests {
+		config := NewConfiguration(test.apiURL, "uuid", "token", true, true, 1, 100, 5)
+		config.httpClient = test.httpClient
+		client := NewClient(config)
+		_, err := client.CreateServer(emptyCtx, ServerCreateRequest{
+				Name:            "test",
+				Memory:          10,
+				Cores:           4,
+				LocationUUID:    dummyUUID,
+				HardwareProfile: DefaultServerHardware,
+				Labels:          []string{"label"},
+			})
 		assert.EqualError(t, err, fmt.Sprintf(test.expectedError, config.apiURL, uri), test.name)
 	}
 }
