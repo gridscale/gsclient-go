5b5b6d3cb0abf65446f77d0930558d9fa8588bdb
Ibrahim Takouna <ibrahim@gridscale.io>
2020-01-20 11:54:47 +0100

    Add network test case for connection refused

--- request_test.go
+++ request_test.go
@@ -12,17 +12,26 @@ import (
 )
 
 type networkTestCase struct {
+	name string
 	apiURL string
 	httpClient *http.Client
 	expectedError string
 }
 
-var networkErrorsConnectionTimeout []networkTestCase = []networkTestCase{
+var networkErrorTests []networkTestCase = []networkTestCase{
 	{
+		name: "Connection timeout",
 		apiURL: defaultAPIURL,
-		httpClient: &http.Client{Timeout: 1 * time.Millisecond},
-		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: net/http: request canceled while waiting for connection (Client.Timeout exceeded while awaiting headers)",
+		httpClient: &http.Client{Timeout: 1 * time.Nanosecond},
+		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: net/http: request canceled (Client.Timeout exceeded while awaiting headers)",
 	},
+	{
+		name: "Connection refused",
+		apiURL: "http://127.0.0.1",
+		httpClient: http.DefaultClient,
+		expectedError: "Maximum number of trials has been exhausted with error: Get %s%s: dial tcp 127.0.0.1:80: connect: connection refused",
+	},
+	
 }
 
 func TestRequest_RetryNetworkErrors(t *testing.T) {
@@ -32,11 +41,11 @@ func TestRequest_RetryNetworkErrors(t *testing.T) {
 	mux.HandleFunc(uri, func(w http.ResponseWriter, r *http.Request) {})
 	defer server.Close()
 	
-	for _, test := range networkErrorsConnectionTimeout {
+	for _, test := range networkErrorTests {
 		config := NewConfiguration(test.apiURL, "uuid", "token", true, true, 1, 100, 5)
 		config.httpClient = test.httpClient
 		client := NewClient(config)
 		_, err := client.GetServer(emptyCtx, dummyUUID)
-		assert.EqualError(t, err, fmt.Sprintf(test.expectedError, config.apiURL, uri))
+		assert.EqualError(t, err, fmt.Sprintf(test.expectedError, config.apiURL, uri), test.name)
 	}
 }
