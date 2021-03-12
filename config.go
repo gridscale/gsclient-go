f9aa64519eb388e847a17109ab8295ac2be0d727
Van Thong Nguyen <tom@gridscale.io>
2021-03-12 12:06:19 +0100

    update version number

--- config.go
+++ config.go
@@ -12,7 +12,7 @@ import (
 const (
 	defaultMaxNumberOfRetries     = 5
 	defaultDelayIntervalMilliSecs = 1000
-	version                       = "3.3.2"
+	version                       = "3.4.1"
 	defaultAPIURL                 = "https://api.gridscale.io"
 	resourceActiveStatus          = "active"
 	requestDoneStatus             = "done"
